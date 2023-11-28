0aacf14efc00567003d115bdbf07fb9ab4621818
Miłosz Kraiński <milek@MacBook-Pro-Miosz.local>
2023-11-28 16:37:09 +0100

    zadanie stozek

---
+++ objetosc-stozka/objetosc-stozka/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // input uzytkownika
+            string[] input = Console.ReadLine().Split();
+            int r = int.Parse(input[0]);
+            int l = int.Parse(input[1]);
+            
+            if (r < 0 || l < 0)
+            {
+                Console.WriteLine("ujemny argument");
+                return;
+            }
+
+            // exception
+            if (r > l)
+            {
+                Console.WriteLine("obiekt nie istnieje");
+                return;
+            }
+
+            // objetosc
+            double h = Math.Sqrt(l * l - r * r);
+            double v = Math.PI * r * r * h / 3;
+            
+            int vFloor = (int)Math.Floor(v);
+            int vCeil = (int)Math.Ceiling(v);
+
+            // wyniki
+            Console.WriteLine($"{Math.Floor(v)} {Math.Ceiling(v)}");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("blad");
+        }
+    }
+}
---
+++ przetwarzanie-tablic-1D/przetwarzanie-tablic-1D/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var a = new int[] {-2, -1, 0, 1, 4};
+var b = new int[] {-2, -1, 0, 1, 4, 5, 6};
+
+static void Print(int[] a, int[] b)
+{
+    string foundedNums = "";
+    for (int indexA = 0; indexA < a.Length; indexA++)
+    {
+        var foundAinB = false;
+        for (int indexB = 0; indexB < b.Length; indexB++)
+        {
+            // Console.WriteLine($"poronuje wlasnie {a[indexA]} z {b[indexB]}");
+            if (a[indexA] == b[indexB])
+            {
+                foundAinB = true;
+                break;
+            }
+        }
+        if (!foundAinB && !foundedNums.Contains(a[indexA].ToString()))
+        {
+            foundedNums += a[indexA] + " ";
+        }
+    }
+
+    if (foundedNums == "")
+    {
+        Console.WriteLine("empty");
+    }
+    else
+    {
+        Console.WriteLine(foundedNums);
+    }
+}
+
+Print(a, b);
