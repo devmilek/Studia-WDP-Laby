b7f6d2d860f471cbde76b32b1710a68fca54cc76
Miłosz Kraiński <milek@MacBook-Pro-Miosz.local>
2023-12-02 14:28:55 +0100

    new tasks

---
+++ wzorki/nomuer-8/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace wzorki
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Print8(5);
+            PrintZ(8);
+        }
+
+        public static void Print8(int width)
+        {
+            // Print uppper
+            for (var i = 0; i < width; i++)
+            {
+                if (i == 0 || i == width)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("*");
+                }
+            }
+        }
+
+        public static void PrintZ(int width)
+        {
+            for (var i = width; i < width; i++)
+            {
+                Console.Write("*");
+            }
+
+            for (var i = 0; i < width - 2; i++)
+            {
+                for (var j = i; i )
+            }
+        }
+    }
+}
+
