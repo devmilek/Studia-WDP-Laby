177a6175b38cedc5c59692956916cae60157204f
Miłosz Kraiński <milek@MacBook-Pro-Miosz.local>
2023-12-04 22:56:30 +0100

    matrix tasks

---
+++ przetwarzanie-tablic-1D/pytanie-3/Program.cs
@@ -0,0 +1,67 @@
+﻿static void Print(int[] a, int[] b)
+{
+    int i = 0, j = 0;
+    bool isEmpty = true;
+    int? lastPrinted = null;
+
+    while (i < a.Length && j < b.Length)
+    {
+        if (a[i] < b[j])
+        {
+            if (!lastPrinted.HasValue || a[i] != lastPrinted.Value)
+            {
+                Console.Write(a[i] + " ");
+                isEmpty = false;
+                lastPrinted = a[i];
+            }
+            i++;
+        }
+        else if (b[j] < a[i])
+        {
+            if (!lastPrinted.HasValue || b[j] != lastPrinted.Value)
+            {
+                Console.Write(b[j] + " ");
+                isEmpty = false;
+                lastPrinted = b[j];
+            }
+            j++;
+        }
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+
+    while (i < a.Length)
+    {
+        if (!lastPrinted.HasValue || a[i] != lastPrinted.Value)
+        {
+            Console.Write(a[i] + " ");
+            isEmpty = false;
+            lastPrinted = a[i];
+        }
+        i++;
+    }
+
+    while (j < b.Length)
+    {
+        if (!lastPrinted.HasValue || b[j] != lastPrinted.Value)
+        {
+            Console.Write(b[j] + " ");
+            isEmpty = false;
+            lastPrinted = b[j];
+        }
+        j++;
+    }
+
+    if (isEmpty)
+    {
+        Console.Write("empty");
+    }
+}
+
+var a = new int[] {0, 1, 1, 2, 3, 3, 3};
+var b = new int[] {0, 1, 2, 3, 3};
+
+Print(a, b);
---
+++ tablice-2d-macierze/pytanie-1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace pytanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var data = input.Split(' ');
+            
+            var rows = int.Parse(data[0]);
+            var cols = int.Parse(data[1]);
+            
+            var matrix = new int[rows, cols];
+
+            for (var i = 0; i < rows; i++)
+            {
+                var row = Console.ReadLine();
+                var rowData = row.Split(' ');
+                
+                for (var j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = int.Parse(rowData[j]);
+                }
+            }
+            
+            var transpozycja = new int[cols, rows];
+            
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < cols; j++)
+                {
+                    transpozycja[j, i] = matrix[i, j];
+                }
+            }
+            
+            for (int i = 0; i < transpozycja.GetLength(0); i++)
+            {
+                for (int j = 0; j < transpozycja.GetLength(1); j++)
+                {
+                    Console.Write(transpozycja[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
